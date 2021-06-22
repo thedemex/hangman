40ab1fb6b20f2df8e039566b0b7d271da4e815d8
Dillon Yu <yudillon8@gmail.com>
2021-06-22 15:45:19 -0700

    added main functionality of game, added Chinese support

--- hangman.py
+++ hangman.py
@@ -7,8 +7,12 @@ ans = []     #the sentence solution
 curAns = []  #the current solution, including the characters guessed by the player so far
 gameOver = False
 
-def change_curAns(tries):
-    print(ans[tries])
+def change_curAns(char):
+    for i in range(len(ans)):
+        if(ans[i] == char):
+            curAns[i] = char
+        
+
 
 def show_hangman(state):
     states = ["""
@@ -76,22 +80,43 @@ def show_hangman(state):
         """]
     print(states[state])
 
+def setup():
+    show_hangman(numWrong)
+    print('   ' + ' '.join(curAns) + '。')
+    print('Please enter another character')
+
 def main():
     global ans
+    global numWrong
 
     #set correct answer
     print('Hello! Welcome to Hangman! Let\'s setup the game!')
     print('Type in the correct answer: ')
-    ans = input()
+    ans = list(input())
     print('Let\'s begin!')
 
-    #game setup
-    for i in range(len(ans)):
+    #initial game setup
+    for i in ans:
         curAns.append('_')
-    
     show_hangman(numWrong)
-    print('   ' + ' '.join(curAns))
+    print('   ' + ' '.join(curAns) + '。')
     print('Please enter a character')
+    
+    #running game
+    while (numWrong < numTries) and (curAns != ans):
+        guess = input()
+        if guess in ans:
+            change_curAns(guess)
+        else:
+            numWrong += 1
+            print('Sorry, that character is not in the answer.')
+        setup()
+    
 
+    if(numWrong < numTries):
+        print('Congratulations! You win!')
+    else:
+        print('\nSorry, you lose. The correct sentence was ' + ''.join(ans) + '。')
 
+    
 main()

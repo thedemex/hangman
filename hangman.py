afcff3c1b1c183afe6866b64070f7a2160745b65
Dillon Yu <yudillon8@gmail.com>
2021-06-22 14:46:29 -0700

    added global vars and hangman states

--- hangman.py
+++ hangman.py
@@ -1,2 +1,77 @@
-print('Hello World')
-print("Hello World!!")
+#import os
+#os.system("pause") to stop the program (Windows only)
+
+numWrong = 0 #number of wrong guesses so far
+numTries = 6 #the total number of tries
+ans = []     #the sentence solution
+curAns = []  #the current solution, including the characters guessed by the player so far
+
+def show_sentence():
+
+def show_hangman(state):
+    states = ["""
+        --------
+        |     |
+        |
+        |
+        |
+        |
+        |_________
+            """, 
+            """
+        --------
+        |     |
+        |     O
+        |
+        |
+        |
+        |_________
+            """,
+             """
+        --------
+        |     |
+        |     O
+        |     |
+        |     |
+        |
+        |_________
+            """,
+             """
+        --------
+        |     |
+        |     O
+        |   \\ |
+        |     |
+        |
+        |_________
+            """,
+              """
+        --------
+        |     |
+        |     O
+        |   \\ | /
+        |     |
+        |
+        |_________
+            """,
+              """
+        --------
+        |     |
+        |     O
+        |   \\ | /
+        |     |
+        |    /
+        |_________
+            """,
+              """
+        --------
+        |     |
+        |     O
+        |   \\ | /
+        |     |
+        |    / \\
+        |_________
+            """]
+    print(states[state])
+
+#show_hangman(6)

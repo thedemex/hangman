d721cd60ef7de6bdd9dd41b7a6a41d13d4522243
Dillon Yu <yudillon8@gmail.com>
2021-06-22 15:18:10 -0700

    inital setup complete and fixed some formatting

--- hangman.py
+++ hangman.py
@@ -5,73 +5,93 @@ numWrong = 0 #number of wrong guesses so far
 numTries = 6 #the total number of tries
 ans = []     #the sentence solution
 curAns = []  #the current solution, including the characters guessed by the player so far
+gameOver = False
 
-def show_sentence():
+def change_curAns(tries):
+    print(ans[tries])
 
 def show_hangman(state):
     states = ["""
-        --------
-        |     |
-        |
-        |
-        |
-        |
-        |_________
-            """, 
-            """
-        --------
-        |     |
-        |     O
-        |
-        |
-        |
-        |_________
-            """,
-             """
-        --------
-        |     |
-        |     O
-        |     |
-        |     |
-        |
-        |_________
-            """,
-             """
-        --------
-        |     |
-        |     O
-        |   \\ |
-        |     |
-        |
-        |_________
-            """,
-              """
-        --------
-        |     |
-        |     O
-        |   \\ | /
-        |     |
-        |
-        |_________
-            """,
-              """
-        --------
-        |     |
-        |     O
-        |   \\ | /
-        |     |
-        |    /
-        |_________
-            """,
-              """
-        --------
-        |     |
-        |     O
-        |   \\ | /
-        |     |
-        |    / \\
-        |_________
-            """]
+    --------
+    |     |
+    |
+    |
+    |
+    |
+    |_________
+        """, 
+        """
+    --------
+    |     |
+    |     O
+    |
+    |
+    |
+    |_________
+        """,
+         """
+    --------
+    |     |
+    |     O
+    |     |
+    |     |
+    |
+    |_________
+        """,
+         """
+    --------
+    |     |
+    |     O
+    |   \\ |
+    |     |
+    |
+    |_________
+        """,
+          """
+    --------
+    |     |
+    |     O
+    |   \\ | /
+    |     |
+    |
+    |_________
+        """,
+          """
+    --------
+    |     |
+    |     O
+    |   \\ | /
+    |     |
+    |    /
+    |_________
+        """,
+          """
+    --------
+    |     |
+    |     O
+    |   \\ | /
+    |     |
+    |    / \\
+    |_________
+        """]
     print(states[state])
 
-#show_hangman(6)
+def main():
+    global ans
+
+    #set correct answer
+    print('Hello! Welcome to Hangman! Let\'s setup the game!')
+    print('Type in the correct answer: ')
+    ans = input()
+    print('Let\'s begin!')
+
+    #game setup
+    for i in range(len(ans)):
+        curAns.append('_')
+    
+    show_hangman(numWrong)
+    print('   ' + ' '.join(curAns))
+    print('Please enter a character')
+
+
+main()
